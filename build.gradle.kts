17f27fcc142f1563adfffda8a30ed66bcafd8082
Barla6 <barlaliko62@gmail.com>
2022-09-01 14:58:17 +0300

    gradle changes to use module in server

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,27 @@
 
-val kotlinVersion = "5.8.2"
+val kotlinVersion = "1.5.20"
 val logbackVersion = "1.2.11"
 
 plugins {
-    application
-    kotlin("jvm") version "1.6.20"
+    id("checkers.kotlin-library-conventions")
+//    application
+//    kotlin("jvm") version "1.6.20"
 }
 
-group = "com.checkers"
-version = "0.0.1"
-application {
-    mainClass.set("com.checkers.ApplicationKt")
-
-    val isDevelopment: Boolean = project.ext.has("development")
-    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
-}
-
-repositories {
-    mavenCentral()
-    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
-}
-
+//group = "com.checkers"
+//version = "0.0.1"
+//application {
+//    mainClass.set("com.checkers.ApplicationKt")
+//
+//    val isDevelopment: Boolean = project.ext.has("development")
+//    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+//}
+//
+//repositories {
+//    mavenCentral()
+//    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
+//}
+//
 dependencies {
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-api:$kotlinVersion")
@@ -30,7 +31,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.3")
     implementation("com.google.code.gson:gson:2.7")
 }
-
-tasks.test {
-    useJUnitPlatform()
-}
+//
+//tasks.test {
+//    useJUnitPlatform()
+//}
